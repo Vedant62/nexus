accb2d8dd0a07a07356196aea124b3bd3c7e0db9
Vedant Sharma <vedantsharma@Vedants-Air.bbrouter>
2024-06-28 14:25:20 +0530

    fixed errors and final commit

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -70,7 +70,7 @@ const Explore = () => {
         {shouldShowSearchResults ? (
           <SearchResults
             isSearchFetching={isSearchFetching}
-            searchedPosts={searchedPosts}
+            searchedPosts={searchedPosts!}
           />
         ) : shouldShowPosts ? (
           <p className="text-light-4  mt-10 text-center w-full">End of posts</p>
